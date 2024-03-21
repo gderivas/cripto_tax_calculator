0c7c5423bc17f821da605a00c8d524d1d1ccaa3e
G. de Rivas <g.drivas@gmail.com>
2024-03-21 10:24:23 +0100

    Execution prints added

--- criptotax/calculator.py
+++ criptotax/calculator.py
@@ -3,7 +3,7 @@ class tax_calculator:
         self.args = args
         self.kraken_path = path[0]
         self.nmr_path = path[1]
-        self.kraken_data, self.nmr_data = self.import_data(args.plattform)
+        self.kraken_data, self.nmr_data = self.import_data()
 
     def import_data(self):
         if self.args.plattform == 'all':
@@ -15,6 +15,7 @@ class tax_calculator:
             df_n = ''
         else:
             print('Plattform not recognized')
+        print('Data import succesfully!\n')
         return df_k,df_n
 
     def calculate(self):
@@ -29,12 +30,21 @@ class tax_calculator:
 
 
     def calculate_nmr(self):
+        print('Calculating Numerai profits/losses:\n')
+        print('#'*10)
+        print('\n')
         pass
 
     def calculate_kraken(self):
+        print('Calculating Kraken profits/losses:\n')
+        print('#'*10)
+        print('\n')
         pass
 
     def summary(self):
+        print('Summary:\n')
+        print('#'*10)
+        print('\n')
         pass
 
     def export(self):
